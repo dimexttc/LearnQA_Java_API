84e224924034b9ed33b03f8a203555a3654b6037
dmitry <rnr-s@inbox.ru>
2025-04-23 01:17:57 +0300

    homework2

---
+++ src/test/java/HomeworkTest.java
@@ -0,0 +1,15 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HomeworkTest {
+
+    @Test
+            public void someTest()
+    {
+        Response response=RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
