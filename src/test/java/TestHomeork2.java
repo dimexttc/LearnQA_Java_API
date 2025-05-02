5f2538ad9e43687f4dde74da32ec88b1f558e952
dmitry <rnr-s@inbox.ru>
2025-05-02 09:11:54 +0300

    homework_get

--- src/test/java/TestHomeork2.java
+++ src/test/java/TestHomeork2.java
@@ -7,17 +7,27 @@ import java.util.Map;
 
 public class TestHomeork2 {
     @Test
-    public void homework_get(){
-        JsonPath answerFull=RestAssured
+    public void Homework_get() {
+        JsonPath response = RestAssured
                 .given()
                 .get("https://playground.learnqa.ru/api/get_json_homework")
                 .jsonPath();
 
-        answerFull.prettyPrint();
-        List<Map<String,String>> whatWeNeed=answerFull.get("messages");
-        System.out.println(whatWeNeed.get(1));
+        response.prettyPrint();
+
+
+        List<Map<String,String>> uniqAnswer= response.get("messages");
+        String check=uniqAnswer.get(1).get("message");
+        String finalResult=check.substring(2,check.length()-3);
+        if (finalResult.equals("d this is a second mess")){
+            System.out.println("The test is passed");
+        }
+        else {
+            System.out.println("The test is failed");
+        }
+
+
 
     }
-
-    }
+}
 
