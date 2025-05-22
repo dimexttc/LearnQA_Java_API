32b006c898d973bfe52b65e3debb800d57c3e18d
dmitry <rnr-s@inbox.ru>
2025-05-22 08:36:45 +0300

    TestHomeWork2_3

---
+++ src/test/java/ex11.java
@@ -0,0 +1,29 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Map;
+
+import static org.hamcrest.Matchers.hasKey;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ex11 {
+
+@Test
+    public void getCookie(){
+        Response response= RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        String expectedCookieKey="HomeW1ork";
+        String expectedCookieValue="hw_value";
+        Map<String,String> cookies=response.getCookies();
+
+    assertAll("Cookie checks",
+            () -> assertTrue(cookies.containsKey(expectedCookieKey),
+                    "Cookie '" + expectedCookieKey + "' not found"),
+            () -> assertEquals(expectedCookieValue, cookies.get(expectedCookieKey),
+                    "Cookie value doesn't match"));
+
+}
+}
