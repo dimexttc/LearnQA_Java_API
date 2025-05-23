5655932fc442ca257623403902aca97470224a07
dmitry <rnr-s@inbox.ru>
2025-05-23 08:26:38 +0300

    TestHomeWork2_3

--- src/test/java/ex13.java
+++ src/test/java/ex13.java
@@ -1,10 +1,7 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ex13 {
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/recourses.txt",delimiter = '|')
+    @CsvFileSource(resources = "/userAgentInfo.txt",delimiter = '|')
     public void checkUserAgent(
             String userAgent,
             String expectedPlatform,
