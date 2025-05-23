aceb90db022c1a4e01af793707b79ce151c35e13
dmitry <rnr-s@inbox.ru>
2025-05-23 08:22:26 +0300

    TestHomeWork2_3

---
+++ src/test/java/ex13.java
@@ -0,0 +1,43 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ex13 {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/recourses.txt",delimiter = '|')
+    public void checkUserAgent(
+            String userAgent,
+            String expectedPlatform,
+            String expectedBrowser,
+            String expectedDevice){
+
+
+
+    JsonPath response=RestAssured
+            .given()
+            .headers("User-Agent",userAgent)
+            .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+            .jsonPath();
+
+        String realDevice=response.getString("device");
+        String realBrowser=response.getString("browser");
+        String realPlatform=response.getString("platform");
+
+
+        assertAll(
+
+                ()->assertEquals(expectedDevice,response.getString("device"),realDevice + " - это неверное устройство"),
+                ()->assertEquals(expectedBrowser,response.getString("browser"),realBrowser + " - это неверный браузер"),
+                ()->assertEquals(expectedPlatform,response.getString("platform"),realPlatform + " - это неверная платформа")
+    );
+    }
+
+}
